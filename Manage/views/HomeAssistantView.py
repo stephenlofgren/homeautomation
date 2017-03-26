ed4e2ad8644c47bf37e47b4c813b958bd15236b4
Stephen Lofgren <stephenlofgren@hotmail.com>
2017-03-26 11:24:48 -0600

    switch to https

--- Manage/views/HomeAssistantView.py
+++ Manage/views/HomeAssistantView.py
@@ -6,28 +6,33 @@ import requests
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
-from rest_framework.decorators import api_view, renderer_classes, permission_classes
+from rest_framework.decorators import api_view
+from rest_framework.decorators import renderer_classes, permission_classes
 from rest_framework import permissions
 from requests.auth import HTTPBasicAuth
+from django.conf import settings
 
 
 class HomeAssistantView(APIView):
     """defines views for each of the http verbs"""
+     
+    endpoint = "https://www.lofgrenguenther.com:8123"
 
     @api_view(['GET'])
     @renderer_classes((JSONRenderer,))
     @permission_classes((permissions.AllowAny,))
     def status(self, pk=None):
 
-        url = "http://192.168.2.17:8123/api/states/%s" % pk
+        url = HomeAssistantView.endpoint + "/api/states/%s" % pk
         headers = {
             'accept': "application/json",
             'content-type': "text/json",
             'x-ha-access': "esp8266Fun",
             'Accept': "application/json"
         }
-
-        response = requests.request("GET", url, headers=headers)
+        static = "static/www.lofgrenguenther.com.pem"
+        static = os.path.join(settings.BASE_DIR, static)
+        response = requests.request("GET", url, headers=headers, verify=static)
 
         message = json.loads(response.text)
         return Response(message)
@@ -39,9 +44,9 @@ class HomeAssistantView(APIView):
         payload = {"entity_id": device_code}
 
         if "light." in device_code:
-            url = "http://192.168.2.17:8123/api/services/light/turn_off"
+            url = HomeAssistantView.endpoint + "/api/services/light/turn_off"
         else:
-            url = "http://192.168.2.17:8123/api/services/switch/turn_off"
+            url = HomeAssistantView.endpoint + "/api/services/switch/turn_off"
 
         headers = {
             'accept': "application/json",
@@ -51,7 +56,8 @@ class HomeAssistantView(APIView):
         }
 
         response = requests.request(
-            "POST", url, headers=headers, data=json.dumps(payload))
+            "POST", url, headers=headers, data=json.dumps(payload),
+            verify=False)
 
         message = json.loads(response.text)
         return Response(message)
@@ -62,10 +68,11 @@ class HomeAssistantView(APIView):
     def turn_on(self, device_code, on_level=255):
 
         if "light." in device_code:
-            url = "http://192.168.2.17:8123/api/services/light/turn_on"
+            url = HomeAssistantView.endpoint + "/api/services/light/turn_on"
             payload = {"entity_id": device_code, "brightness": on_level}
         else:
-            url = "http://192.168.2.17:8123/api/services/switch/turn_on"
+            url = "https://www.lofgrenguenther.com:8123"
+            url += "/api/services/switch/turn_on"
             payload = {"entity_id": device_code}
 
         headers = {
@@ -76,8 +83,8 @@ class HomeAssistantView(APIView):
         }
 
         response = requests.request(
-            "POST", url, headers=headers, data=json.dumps(payload))
+            "POST", url, headers=headers, data=json.dumps(payload),
+            verify=False)
 
         message = json.loads(response.text)
         return Response(message)
-
